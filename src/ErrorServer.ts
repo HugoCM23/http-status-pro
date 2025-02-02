6e49a916707110395f5497912f4369b81210e32f
Victor Hugo Martinez <hugocm@MacBook-Air-de-Victor.local>
2025-02-01 19:23:16 -0600

    Actulizacion a version 1.0.6

--- src/ErrorServer.ts
+++ src/ErrorServer.ts
@@ -1,24 +1,24 @@
 // src/ErrorServer.ts
 export class ErrorServer extends Error {
-    public code: number;
-    public error: string;
-  
-    private readonly errors: Record<string, { code: number; message: string }> = {
-      SERVER: { code: 500, message: 'Internal Server Error' },
-      DRIVER: { code: 503, message: 'Service Unavailable' },
-      BAD_REQUEST: { code: 400, message: 'Bad Request' },
-      UNAUTHORIZED: { code: 401, message: 'Unauthorized' },
-      NOT_FOUND: { code: 404, message: 'Not Found' },
-      EXIST: { code: 409, message: 'Conflict' },
-    };
-  
-    constructor(type: string, message?: string) {
-      super(message || 'without trace');
-      const errorType = this.errors[type];
-      if (!errorType) {
-        throw new Error(`Error type "${type}" not found.`);
-      }
-      this.code = errorType.code;
-      this.error = errorType.message;
+  public code: number;
+  public error: string;
+
+  private readonly errors: Record<string, { code: number; message: string }> = {
+    SERVER: { code: 500, message: 'Internal Server Error' },
+    DRIVER: { code: 503, message: 'Service Unavailable' },
+    BAD_REQUEST: { code: 400, message: 'Bad Request' },
+    UNAUTHORIZED: { code: 401, message: 'Unauthorized' },
+    NOT_FOUND: { code: 404, message: 'Not Found' },
+    EXIST: { code: 409, message: 'Conflict' },
+  };
+
+  constructor(type: string, message?: string) {
+    super(message || 'without trace');
+    const errorType = this.errors[type];
+    if (!errorType) {
+      throw new Error(`Error type "${type}" not found.`);
     }
+    this.code = errorType.code;
+    this.error = errorType.message;
   }
+}
--- src/StatusServer.ts
+++ src/StatusServer.ts
@@ -1,21 +1,21 @@
 // src/StatusServer.ts
 export class StatusServer {
-    private readonly status: Record<string, { code: number; message: string }> = {
-      CREATED: { code: 201, message: 'Resource Created Successfully' },
-      UPDATED: { code: 200, message: 'Resource Updated Successfully' },
-      DELETED: { code: 200, message: 'Resource Deleted Successfully' },
-      ALREADY_EXISTS: { code: 409, message: 'Resource Already Exists' },
-      INVALID: { code: 422, message: 'Invalid Data Provided' },
-    };
-  
-    public getStatus(type: string, message?: string) {
-      const statusType = this.status[type];
-      if (!statusType) {
-        throw new Error(`Status type "${type}" not found.`);
-      }
-      return {
-        code: statusType.code,
-        message: message || statusType.message,
-      };
+  private readonly status: Record<string, { code: number; message: string }> = {
+    CREATED: { code: 201, message: 'Resource Created Successfully' },
+    UPDATED: { code: 200, message: 'Resource Updated Successfully' },
+    DELETED: { code: 200, message: 'Resource Deleted Successfully' },
+    ALREADY_EXISTS: { code: 409, message: 'Resource Already Exists' },
+    INVALID: { code: 422, message: 'Invalid Data Provided' },
+  };
+
+  public getStatus(type: string, message?: string) {
+    const statusType = this.status[type];
+    if (!statusType) {
+      throw new Error(`Status type "${type}" not found.`);
     }
+    return {
+      code: statusType.code,
+      message: message || statusType.message,
+    };
   }
+}
